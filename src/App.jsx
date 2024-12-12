82349b6e8f541993def6b648083dbf903bbaecb5
Aditi Kala <aditi25.kala@gmail.com>
2024-12-12 12:26:32 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+
+const App = () => {
+  return (
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-0 left-0 -z-10 h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="absolute top-0 left-0 z-[-2] min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="container mx-auto px-12">
+        <Nav />
+        <div className="pt-20 m-8"> 
+          <Home/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { Link as ScrollLink } from "react-scroll";
+
+export default function Nav() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+    useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <section id="Navbar">
+        <nav
+        className={`fixed top-0 left-0 right-0 z-10 flex items-center justify-between p-3 px-6 transition-all duration-300 ${
+            isScrolled ? "bg-neutral-950 bg-opacity-60 shadow-md" : "bg-transparent"
+        }`}
+        >
+        <div className="flex flex-shrink-0 items-center m-4">
+            <h1 className="text-5xl font-bold">AK</h1>
+            </div>
+        <div className="flex items-center gap-8 text-lg text-white">
+            <ScrollLink
+            to="/"
+            smooth={true}
+            duration={500}
+            className="cursor-pointer hover:text-gray-400"
+            onClick={scrollToTop}
+            >
+            Home
+            </ScrollLink>
+            <ScrollLink
+            to="about"
+            smooth={true}
+            duration={500}
+            className="cursor-pointer hover:text-gray-400"
+            >
+            About
+            </ScrollLink>
+            <ScrollLink
+            to="contact"
+            smooth={true}
+            duration={500}
+            className="cursor-pointer hover:text-gray-400"
+            >
+            Contact
+            </ScrollLink>
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl text-white">
+            <a href="https://github.com/why-aditi" target="_blank" rel="noopener noreferrer">
+            <FaGithub aria-label="GitHub" />
+            </a>
+            <a
+            href="https://www.linkedin.com/in/aditi-kala-b4740228b/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <FaLinkedin aria-label="LinkedIn" />
+            </a>
+            <a
+            href="https://www.instagram.com/lostintheskyie/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <FaInstagram aria-label="Instagram" />
+            </a>
+        </div>
+        </nav>
+    </section>
+  );
+}
