0d199b3346a07227419e43e1aeadb4ad75f03394
Aditi Kala <aditi25.kala@gmail.com>
2025-08-01 11:27:21 +0530

    Edit Resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -77,7 +77,7 @@ export default function Home() {
                 <FaArrowRight className="group-hover:translate-x-1 transition-transform" />
               </a>
               <a 
-                href="https://drive.google.com/file/d/1odQDIL3M0sSpww2PnJBJcLNzBXClh62j/view?usp=sharing" 
+                href="https://drive.google.com/file/d/1IIEbnOMA0780dfHf1enAqZYxdCv57lP-/view?usp=sharing" 
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-3 bg-neutral-800/50 backdrop-blur-sm border border-neutral-700 hover:border-indigo-500/50 text-white font-medium rounded-lg transition-all"
