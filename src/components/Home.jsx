4349ea3627a298a3733d8c2ae0e35c21c26b5ff4
Aditi Kala <130339327+why-aditi@users.noreply.github.com>
2026-01-14 00:54:53 +0530

    Update link to Google Drive file in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -77,7 +77,7 @@ export default function Home() {
                 <FaArrowRight className="group-hover:translate-x-1 transition-transform" />
               </a>
               <a 
-                href="https://drive.google.com/file/d/1mHOBFeUne0dZMB7dT3X7GsW2dtGTgUbp/view?usp=sharing" 
+                href="https://drive.google.com/file/d/1ok4XmaY08Rd0WiZBjNXcfFdsbE-DgxNi/view?usp=sharing" 
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-3 bg-neutral-800/50 backdrop-blur-sm border border-neutral-700 hover:border-indigo-500/50 text-white font-medium rounded-lg transition-all"
